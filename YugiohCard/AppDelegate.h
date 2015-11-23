1451f4a3b4632d59cb6dc2d6aa8b7961ad6c5fba
T√πng Ho√†ng <tungmdc55@gmail.com>
2015-11-23 15:29:17 +0700

    Initial Commit

---
+++ YugiohCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YugiohCard
+//
+//  Created by Hoang Ngoc Tung on 11/23/15.
+//  Copyright © 2015 HoangNgocTung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YugiohCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YugiohCard
+//
+//  Created by Hoang Ngoc Tung on 11/23/15.
+//  Copyright © 2015 HoangNgocTung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
